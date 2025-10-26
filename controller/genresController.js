8025d227e1a725e398d9013f52a8607d142fa264
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-10-26 15:39:24 +0800

    feat: add a route and controller for updating and deleting genre

--- controller/genresController.js
+++ controller/genresController.js
@@ -1,9 +1,37 @@
 const db = require("../db/queries");
 
 async function getGenreBooks(req, res) {
-  const { genre } = req.params;
-  const books = await db.getGenreBooks(genre);
-  res.render("books", { books, genreId: genre });
+  const { genreId } = req.params;
+  const books = await db.getGenreBooks(genreId);
+  res.render("books", { books, genreId });
 }
 
-module.exports = { getGenreBooks };
+async function renderUpdateGenreForm(req, res) {
+  const { genreId } = req.params;
+  const genreDetails = await db.getGenre(genreId);
+  res.render("updateGenres", {
+    genreId,
+    genreDetails: genreDetails[0],
+  });
+}
+
+async function updateGenre(req, res) {
+  const { genreId } = req.params;
+  const { genre } = req.body;
+
+  await db.updateGenre(genreId, genre);
+  res.redirect("/genres");
+}
+
+async function deleteGenre(req, res) {
+  const { genreId } = req.params;
+  await db.deleteGenre(genreId);
+  res.redirect("/genres");
+}
+
+module.exports = {
+  getGenreBooks,
+  deleteGenre,
+  renderUpdateGenreForm,
+  updateGenre,
+};
--- routes/genresRoute.js
+++ routes/genresRoute.js
@@ -1,7 +1,15 @@
 const { Router } = require("express");
-const { getGenreBooks } = require("../controller/genresController");
+const {
+  getGenreBooks,
+  deleteGenre,
+  renderUpdateGenreForm,
+  updateGenre,
+} = require("../controller/genresController");
 const genresRouter = Router();
 
-genresRouter.get("/:genre", getGenreBooks);
+genresRouter.get("/genres/:genreId", getGenreBooks);
+genresRouter.post("/delete/:genreId", deleteGenre);
 
+genresRouter.get("/updateGenre/:genreId", renderUpdateGenreForm);
+genresRouter.post("/updateGenre/:genreId", updateGenre);
 module.exports = genresRouter;
